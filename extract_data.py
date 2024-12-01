c45a74018eb4aa8d4d5611d6398b7b63539ca00f
azurstarshine <155862691+azurstarshine@users.noreply.github.com>
2024-12-01 18:15:47 -0500

    Ensuring JSON files have unix-style line endings

--- extract_data.py
+++ extract_data.py
@@ -633,7 +633,7 @@ def to_json_serializable(o):
 
 
 def write_pvp_json_data(path: Path, data: Any):
-    with open(path, 'w', encoding='utf-8') as f:
+    with open(path, 'w', encoding='utf-8', newline='') as f:
         json.dump(data, f, indent=4, default=to_json_serializable)
         print('Wrote', f.name)
 
