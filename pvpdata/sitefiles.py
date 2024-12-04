55c80e564b40b7b221997650ee7083381c08b4be
azurstarshine <155862691+azurstarshine@users.noreply.github.com>
2024-12-04 04:56:55 -0500

    Moving JSON conversion logic into sitefiles

--- pvpdata/sitefiles.py
+++ pvpdata/sitefiles.py
@@ -3,16 +3,19 @@ Functions and information for working with the site's data files.
 """
 
 from collections.abc import Mapping
+import dataclasses
+from enum import Enum
 import json
 from pathlib import Path
 from types import MappingProxyType
 from typing import Any
 
 from . import DATA_DIR
+from .types import EquipWithRank
 from .types import Equipment
 from .types import Ship
 from .types import ShipUsage
-from .types import to_json_serializable
+
 
 DATA_FILE_BASENAMES : Mapping[type, str] = MappingProxyType({
     Ship: 'ship',
@@ -24,6 +27,28 @@ DATA_FILE_BASENAMES : Mapping[type, str] = MappingProxyType({
 def get_data_path(datatype: type):
     basename = DATA_FILE_BASENAMES[datatype]
     return DATA_DIR / f'{basename}.json'
+
+
+def to_json_serializable(o):
+    # TODO: Simplify these data types to use the names instead of
+    # full objects
+    if isinstance(o, EquipWithRank):
+        return {'name': o.equip.name, 'rank': str(o.rank)}
+
+    if isinstance(o, ShipUsage):
+        return {
+            'ship': o.ship.name,
+            'description': o.description,
+            'equipment': o.slots,
+        }
+
+    if dataclasses.is_dataclass(o):
+        return dataclasses.asdict(o)
+
+    if isinstance(o, Enum):
+        return o.name
+
+    raise TypeError(f'Cannot serialize {o} {type(o).__name__})')
 
 
 def write_pvp_json_data(path: Path, data: Any):
--- pvpdata/types.py
+++ pvpdata/types.py
@@ -3,7 +3,6 @@ Data types for data represented in the site's files.
 """
 
 from collections import defaultdict
-import dataclasses
 from dataclasses import dataclass
 from dataclasses import field
 import enum
@@ -279,23 +278,3 @@ class ShipUsage:
         ])
 
         return f'{type(self).__name__}({data_repr})'
-
-
-def to_json_serializable(o):
-    if isinstance(o, EquipWithRank):
-        return {'name': o.equip.name, 'rank': str(o.rank)}
-
-    if isinstance(o, ShipUsage):
-        return {
-            'ship': o.ship.name,
-            'description': o.description,
-            'equipment': o.slots,
-        }
-
-    if dataclasses.is_dataclass(o):
-        return dataclasses.asdict(o)
-
-    if isinstance(o, Enum):
-        return o.name
-
-    raise TypeError(f'Cannot serialize {o} {type(o).__name__})')
