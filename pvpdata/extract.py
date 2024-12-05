b2ab48cf16a57981134ec81c079be88defe5fba9
azurstarshine <155862691+azurstarshine@users.noreply.github.com>
2024-12-04 20:17:42 -0500

    Replacing double for-loop with single to reduce indentation

--- pvpdata/extract.py
+++ pvpdata/extract.py
@@ -6,7 +6,9 @@ This program reads from an HTML export of the spreadsheet. It is only tested
 against an export from LibreOffice 24.8.2.1.
 """
 
+from collections.abc import Iterator
 from collections.abc import Mapping
+from dataclasses import dataclass
 import json
 from operator import attrgetter
 from time import sleep
@@ -41,6 +43,21 @@ PAGE_NAME_FIXES = {
 }
 
 
+@dataclass(frozen=True)
+class CellLocation:
+    row: int
+    column: int
+
+    def __str__(self):
+        return f'({self.row}, {self.column})'
+
+
+def table_cells(table: bs4.Tag) -> Iterator[tuple[CellLocation, bs4.Tag]]:
+    for rownum, row in enumerate(table.find_all('tr'), start=1):
+        for colnum, cell in enumerate(row.find_all('td'), start=1):
+            yield CellLocation(row=rownum, column=colnum), cell
+
+
 def extract_page_name(wikiurl: UrlParseResult) -> str:
     return urlunquote(wikiurl.path.removeprefix('/').removeprefix('wiki').removeprefix('/'))
 
@@ -72,104 +89,103 @@ def parse_equip_table(
     failures = []
     current_usage = None
 
-    for rownum, row in enumerate(table.find_all('tr'), start=1):
-        for colnum, cell in enumerate(row.find_all('td'), start=1):
-            try:
-                link_children = cell.find_all('a')
+    for loc, cell in table_cells(table):
+        try:
+            link_children = cell.find_all('a')
 
-                if len(link_children) == 1:
-                    urltext: str = link_children[0].attrs['href']
-                    nickname: str = link_children[0].text
+            if len(link_children) == 1:
+                urltext: str = link_children[0].attrs['href']
+                nickname: str = link_children[0].text
 
-                    url: UrlParseResult = urlparse(urltext)
+                url: UrlParseResult = urlparse(urltext)
 
-                    raw_page_name = extract_page_name(url)
-                    page_name = PAGE_NAME_FIXES.get(raw_page_name, raw_page_name)
-                    lazypage = LazyValue(lambda: client.page(page_name, auto_suggest=False))
+                raw_page_name = extract_page_name(url)
+                page_name = PAGE_NAME_FIXES.get(raw_page_name, raw_page_name)
+                lazypage = LazyValue(lambda: client.page(page_name, auto_suggest=False))
 
-                    # Use a generator function to avoid loading the page if not needed
-                    def names():
-                        if page_name != raw_page_name:
-                            yield raw_page_name
-                        yield page_name
-                        # Resolves wiki redirects
-                        yield lazypage.value.title
+                # Use a generator function to avoid loading the page if not needed
+                def names():
+                    if page_name != raw_page_name:
+                        yield raw_page_name
+                    yield page_name
+                    # Resolves wiki redirects
+                    yield lazypage.value.title
 
-                    page_data, cached = cache.get(
-                        names(),
-                        # Must NOT use partial to ensure page is loaded lazily
-                        lambda: load_external_data(ship_skin_data, lazypage.value)
-                    )
+                page_data, cached = cache.get(
+                    names(),
+                    # Must NOT use partial to ensure page is loaded lazily
+                    lambda: load_external_data(ship_skin_data, lazypage.value)
+                )
 
-                    loaded_fragment = urlparse(page_data.url).fragment
-                    if url.fragment != loaded_fragment:
-                        if not loaded_fragment:
-                            warnings.warn(f'url fragment lost: {url.fragment}')
-                        elif not url.fragment:
-                            warnings.warn(f'url fragment added: {loaded_fragment}')
-                        else:
-                            warnings.warn(f'url fragment changed: {url.fragment} to {loaded_fragment}')
-
-                    if isinstance(page_data, Ship):
-                        if current_usage:
-                            current_usage.sort_slots()
-                            current_usage.validate()
-                            usages.append(current_usage)
-                            print('Completed ship usage', current_usage)
-                            sleep(1)
-
-                        current_usage = ShipUsage(page_data)
-                        print()
-                    elif isinstance(page_data, Equipment):
-                        if current_usage:
-                            slot = colnum // 2
-                            if slot in (4,5):
-                                slot = 'aux'
-
-                            rank = EQUIP_RANK_BY_COLOR[cell.attrs['bgcolor'].lower()]
-
-                            current_usage.slots[slot].append(EquipWithRank(page_data, rank))
-                        else:
-                            warnings.warn(f'Found equipment outside ship: {page_data.name}')
-
-                    print(rownum, colnum, page_data, '(cached)' if cached else '')
-                elif current_usage:
-                    if cell.attrs.get('data-sheets-formula', '').lower().startswith('=image'):
-                        print(rownum, colnum, 'is an image')
-                    elif (
-                        cell.text
-                        and not link_children
-                        and (data_sheets_val := extract_data_sheets_value(cell))
-                    ):
-                        # Not empty, no link, not an image, and has JSON in value attr.
-                        # Must be description?
-
-                        if current_usage.description:
-                            # Programming error. Need to distinguish description better.
-                            raise Exception(f'Treating cell at ({rownum}, {colnum}) as second description for {current_usage.ship.name}')
-
-                        # Using the parsed JSON from data-sheets-value preserves all newlines
-                        # and other characters the HTML escapes without having to transform it back.
-                        # Convert manual bullets to Markdown list
-                        current_usage.description = data_sheets_val.replace('\u2022', '*')
-                        print(rownum, colnum, 'Description:', current_usage.desc_preview)
-                    elif not cell.text:
-                        # Check this last to avoid accidentally missing other possibilities
-                        # At present, images have an error message as the cell value, but this could
-                        # potentially change to an empty value later, so check attribute based
-                        # possibilities first.
-                        print(rownum, colnum, 'is empty')
+                loaded_fragment = urlparse(page_data.url).fragment
+                if url.fragment != loaded_fragment:
+                    if not loaded_fragment:
+                        warnings.warn(f'url fragment lost: {url.fragment}')
+                    elif not url.fragment:
+                        warnings.warn(f'url fragment added: {loaded_fragment}')
                     else:
-                        # Inside a ship, but no idea what this cell contains
-                        raise NotImplementedError(f'Unrecognized cell content at ({rownum}, {colnum})')
+                        warnings.warn(f'url fragment changed: {url.fragment} to {loaded_fragment}')
+
+                if isinstance(page_data, Ship):
+                    if current_usage:
+                        current_usage.sort_slots()
+                        current_usage.validate()
+                        usages.append(current_usage)
+                        print('Completed ship usage', current_usage)
+                        sleep(1)
+
+                    current_usage = ShipUsage(page_data)
+                    print()
+                elif isinstance(page_data, Equipment):
+                    if current_usage:
+                        slot = loc.column // 2
+                        if slot in (4,5):
+                            slot = 'aux'
+
+                        rank = EQUIP_RANK_BY_COLOR[cell.attrs['bgcolor'].lower()]
+
+                        current_usage.slots[slot].append(EquipWithRank(page_data, rank))
+                    else:
+                        warnings.warn(f'Found equipment outside ship: {page_data.name}')
+
+                print(loc, page_data, '(cached)' if cached else '')
+            elif current_usage:
+                if cell.attrs.get('data-sheets-formula', '').lower().startswith('=image'):
+                    print(loc, 'is an image')
+                elif (
+                    cell.text
+                    and not link_children
+                    and (data_sheets_val := extract_data_sheets_value(cell))
+                ):
+                    # Not empty, no link, not an image, and has JSON in value attr.
+                    # Must be description?
+
+                    if current_usage.description:
+                        # Programming error. Need to distinguish description better.
+                        raise Exception(f'Treating cell at {loc} as second description for {current_usage.ship.name}')
+
+                    # Using the parsed JSON from data-sheets-value preserves all newlines
+                    # and other characters the HTML escapes without having to transform it back.
+                    # Convert manual bullets to Markdown list
+                    current_usage.description = data_sheets_val.replace('\u2022', '*')
+                    print(loc, 'Description:', current_usage.desc_preview)
+                elif not cell.text:
+                    # Check this last to avoid accidentally missing other possibilities
+                    # At present, images have an error message as the cell value, but this could
+                    # potentially change to an empty value later, so check attribute based
+                    # possibilities first.
+                    print(loc, 'is empty')
                 else:
-                    print(rownum, colnum, 'No ship found yet')
-            except Exception as ex:
-                print('Error:', rownum, colnum, cell, ex)
-                sleep(5)
-                failures.append((rownum, colnum, cell, current_usage, ex))
-                # Skip over current ship
-                current_usage = None
+                    # Inside a ship, but no idea what this cell contains
+                    raise NotImplementedError(f'Unrecognized cell content at {loc}')
+            else:
+                print(loc, 'No ship found yet')
+        except Exception as ex:
+            print('Error:', loc, cell, ex)
+            sleep(5)
+            failures.append((loc, cell, current_usage, ex))
+            # Skip over current ship
+            current_usage = None
 
     return usages, failures
 
